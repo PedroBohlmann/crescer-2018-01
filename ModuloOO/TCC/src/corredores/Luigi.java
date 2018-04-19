ae2573593d4f13a6682cc7758e025eda69239446
Pedro Bohlmann Cascaes Silva <pedro.silva@cwi.com.br>
2018-04-18 21:36:32 -0300

    feat(tcc): Adicionando LuigiTest

--- ModuloOO/TCC/src/corredores/Luigi.java
+++ ModuloOO/TCC/src/corredores/Luigi.java
@@ -1,4 +1,25 @@
 package corredores;
 
-public class Luigi {
+import pistas.Casa;
+import pistas.Pista;
+
+public class Luigi extends Corredor {
+    public Luigi() {
+        super(5);
+    }
+
+    @Override
+    public void andar() {
+        Pista pista = this.getPistaQuePertence();
+        Casa casa = this.getCasaAtual();
+        int quantidadeDeCasas = this.getQuantidadeDeCasasQuePercorre();
+        if (pista != null) {
+            int numeroDaCasaAtual = casa.getNumeroDaCasa();
+            if ((numeroDaCasaAtual + quantidadeDeCasas) <= pista.getTamanhoDaPista()) {
+                setCasaAtual(new Casa(numeroDaCasaAtual + quantidadeDeCasas));
+            } else {
+                pista.adicionarAoPodium(this);
+            }
+        }
+    }
 }
---
+++ ModuloOO/TCC/src/tests/LuigiTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import corredores.Luigi;
+import org.junit.jupiter.api.Test;
+import pistas.Casa;
+import pistas.DonutPlains;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LuigiTest {
+    @Test
+    public void luigiTem5DeVida() {
+        Luigi luigi = new Luigi();
+        assertEquals(5, luigi.getVida());
+    }
+
+    @Test
+    public void luigiAnda6CasasQuandoAndaDuasVezesNaDonutPlains() {
+        Luigi luigi = new Luigi();
+        DonutPlains donutPlains = new DonutPlains();
+
+        donutPlains.adicionarCorredor(luigi);
+
+        luigi.andar();
+        luigi.andar();
+
+        Casa casa=donutPlains.getCasaOndeCorredorEsta(luigi);
+
+        assertEquals(6,casa.getNumeroDaCasa());
+    }
+}
