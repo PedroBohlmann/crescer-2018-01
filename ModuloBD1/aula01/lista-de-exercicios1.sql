7297058412dd2f8fdd32ca8ef284600becafd949
Pedro Bohlmann Cascaes Silva <pedro.silva@cwi.com.br>
2018-04-24 14:40:07 +0000

    feat(aula1): Adicionando lista-de-exercicios2

--- ModuloBD1/aula01/lista-de-exercicios1.sql
+++ ModuloBD1/aula01/lista-de-exercicios1.sql
@@ -1,26 +1,25 @@
--- Exercicio 1
 Create table Produto(
-    IDProduto int identify(1,1) not null,
+    ID int identity(1,1) not null,
     Nome varchar(30) not null,
     NomeDescritivo varchar(100),
     DataCriacao datetime not null,
     LocalEstoque varchar(50),
     Quantidade int,
     Preco float
-);
+	);
 -- Exercicio 2
 Create table LocalEstoque(
-    ID int not null,
+    ID int identity(1,1) not null,
     Nome varchar(50) not null,
     DataCriacao datetime not null,
     Capacidade int
 );
 -- Exercicio 3
-Alter table Produto alter column LocalDoEstoque int;
+Alter table Produto alter column LocalEstoque int;
 -- Exercicio 4
 exec sp_RENAME 'Produto.LocalEstoque' , 'IdEstoque', 'COLUMN'
 -- Exercicio 5
 Alter table Produto Add Constraint PK_Produto primary key (Id);
-Alter table Estoque Add Constraint PK_Estoque primary key (Id);
+Alter table LocalEstoque Add Constraint PK_Estoque primary key (Id);
 -- Exercicio 6
-Alter table Produto Add constraint FK_Produto_Estoque Foreign Key (IdEstoque) References Estoque (Id)
+Alter table Produto Add constraint FK_Produto_Estoque Foreign Key (IdEstoque) References LocalEstoque (Id)
--- ModuloBD1/aula01/lista-de-exercicios2.sql
+++ ModuloBD1/aula01/lista-de-exercicios2.sql
@@ -1,3 +1,15 @@
 -- Exercicio 1
-Select * from Cidade where 1=2;
+Select * into CidadeAux from Cidade;
 -- Exercicio 2
+Truncate Table CidadeAux;
+
+Insert Into CidadeAux(IDCidade,Nome,UF)
+Select IDCidade,Nome,UF from Cidade where UF='RS'
+-- Exercicio 3
+Insert into LocalEstoque(Nome,DataCriacao,Capacidade) values('Estocao',convert(datetime, '1997/05/17', 111),45);
+-- Exercicio 4
+Insert into Produto(Nome,NomeDescritivo,DataCriacao,IdEstoque,Quantidade,Preco) 
+values ('Caixa','Guarda coisas',convert(datetime, '1997/06/22', 111),1,80,45);
+
+Insert into Produto(Nome,NomeDescritivo,DataCriacao,IdEstoque,Quantidade,Preco) 
+values ('Carro','Anda',convert(datetime, '1997/08/05', 111),1,5,45000);
