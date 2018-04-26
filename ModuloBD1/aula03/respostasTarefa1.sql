81e022131405cf78cd3b19a6a112c95abd095bc7
Pedro Bohlmann Cascaes Silva <pedro.silva@cwi.com.br>
2018-04-26 13:13:51 +0000

    feat(aula03): Adicionando respostasTarefa1

---
+++ ModuloBD1/aula03/respostasTarefa1.sql
@@ -0,0 +1,13 @@
+-- Exercicio 1
+Select Empregado, Count(1) from Projeto group by Empregado;
+-- Exercicio 2
+Select Distinct ValorFaturado from Projeto where ValorFaturado>ValorRealizado;
+-- Exercicio 3
+Select Distinct((ValorRealizado-ValorPrevisto)/ValorPrevisto)*100 
+from Projeto where ValorRealizado>ValorPrevisto
+-- Exercicio 4
+Select Distinct Empregado, 
+(Case when CHARINDEX(' ',Empregado)=-1 then Empregado
+	else SUBSTRING(Empregado, 1,CHARINDEX(' ',Empregado))
+end)
+from Projeto
