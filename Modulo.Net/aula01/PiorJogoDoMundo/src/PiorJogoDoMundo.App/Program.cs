1b570437fbe06cac933b8e74df991420f4d18fc8
Pedro Bohlmann Cascaes Silva <pedro.silva@cwi.com.br>
2018-05-24 17:11:20 -0300

    feat(Modulo.Net): commit inicial

---
+++ Modulo.Net/aula01/PiorJogoDoMundo/src/PiorJogoDoMundo.App/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace PiorJogoDoMundo.App
+{
+    class Program
+    {
+
+        public static void PrintMatrix(char [,]matrix){
+            Console.SetCursorPosition(0, 0);
+            for(int i=0;i<matrix.GetLength(0);i++){
+                for(int j=0;j<matrix.GetLength(1);j++){
+                    Console.Write(matrix[i,j]);
+                }
+                Console.WriteLine(' ');
+            }
+        }
+
+        public static Boolean CheckPostionIsValid(char [,]matrix,int x,int y){
+            return (matrix[x,y]!='X');
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var matrix=new char[10,20];
+
+            for(int i=0;i<matrix.GetLength(0);i++){
+                for(int j=0;j<matrix.GetLength(1);j++){
+                    if((i==0)||(i==matrix.GetLength(0)-1)||((j==0)||(j==matrix.GetLength(1)-1))){
+                        matrix.SetValue('X',i,j);
+                    }
+                }
+            }
+
+            var x=1;
+            var y=1;
+
+            matrix.SetValue('O',x,y);
+
+            ConsoleKey key;
+
+            Console.Clear();
+            do{
+                PrintMatrix(matrix);
+
+                key=Console.ReadKey(true).Key;
+                switch(key){
+                    case ConsoleKey.UpArrow:
+                        if(CheckPostionIsValid(matrix,x-1,y)){
+                            matrix.SetValue(' ',x--,y);
+                            matrix.SetValue('O',x,y);
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if(CheckPostionIsValid(matrix,x+1,y)){
+                            matrix.SetValue(' ',x++,y);
+                            matrix.SetValue('O',x,y);
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if(CheckPostionIsValid(matrix,x,y+1)){
+                            matrix.SetValue(' ',x,y++);
+                            matrix.SetValue('O',x,y);
+                        }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if(CheckPostionIsValid(matrix,x,y-1)){
+                            matrix.SetValue(' ',x,y--);
+                            matrix.SetValue('O',x,y);
+                        }
+                        break;
+                    
+                }
+
+            }while(key!=ConsoleKey.Spacebar);
+        }
+    }
+}
---
+++ Modulo.Net/aula01/aula01/src/aula01.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula01.App
+{
+    class Program
+    {
+        public static int SomaValores(int val1,int val2){
+            return val1+val2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SomaValores(1,2));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
