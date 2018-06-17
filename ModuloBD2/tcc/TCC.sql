ae09b5c4ce351cdaa8f49347e93910b8c74e6f27
Pedro Bohlmann Cascaes Silva <pedro.silva@cwi.com.br>
2018-06-17 04:16:24 +0000

    feat(tcc): adicionando tcc completo

--- ModuloBD2/tcc/TCC.sql
+++ ModuloBD2/tcc/TCC.sql
@@ -1,15 +1,3 @@
-Impdp system@xe schemas=MEGASENA directory=ORACLE_DUMP dumpfile=MEGASENA logfile=ImportMEGASENA
-
-SELECT * FROM CONCURSO C
-WHERE C.IDCONCURSO = (SELECT MAX(IDCONCURSO) FROM CONCURSO);
-
-SELECT AP.IDCONCURSO,SUM(AP.VALOR),COUNT(1)
-FROM APOSTA AP
-WHERE AP.IDCONCURSO = (SELECT MAX(IDCONCURSO) FROM CONCURSO)
-GROUP BY AP.IDCONCURSO; 
-
--- O QUE EU FIZ
-
 create or replace package body pck_megasena is
 
   /* Busca valores percentuais conforme regra definida na tabela 'Regra_Rateio_Premio' */
@@ -98,6 +86,9 @@ create or replace package body pck_megasena is
       DBMS_OUTPUT.PUT_LINE('valor total:' ||vValorTotal ||'|id concurso'|| vIdConcurso);
       
       salvaConcurso(vIdConcurso,sysdate,vValorTotal);
+    EXCEPTION
+        WHEN no_data_found THEN
+            DBMS_OUTPUT.PUT_LINE('Sem vendas no ultimo concurso');
       
    end geraProximoConcurso;
   ---------------------------------------------------------------------------------------------------------------------------------------
@@ -105,8 +96,146 @@ create or replace package body pck_megasena is
      Questão "B" - implementar rotina que irá inserir todos os acertadores de um determinado concurso
     */
   procedure atualizaAcertadores (pConcurso in integer) as
+    vIdAposta INTEGER;
+    CURSOR C_ListaDeApostas IS
+        SELECT * FROM APOSTA WHERE IDCONCURSO=pConcurso;
+        
+    CURSOR C_NumerosAposta IS
+        SELECT *  FROM NUMERO_APOSTA N WHERE N.IDAPOSTA=vIdAposta;
+        
+    CURSOR C_ApostasDoConcurso IS
+        SELECT * FROM APOSTA P
+            INNER JOIN APOSTA_PREMIADA PP ON P.IDAPOSTA=PP.IDAPOSTA
+            WHERE P.IDCONCURSO= pConcurso;
+        
+    vPrimeiraDezena NUMBER;
+    vSegundaDezena NUMBER;
+    vTerceiraDezena NUMBER;
+    vQuartaDezena NUMBER;
+    vQuintaDezena NUMBER;
+    vSextaDezena NUMBER;
+    vNumeroDeAcertosPorAposta INTEGER:=0;
+    
+    vNumeroTotalDeAcertosQuadra INTEGER:=0;
+    vNumeroTotalDeAcertosQuina INTEGER:=0;
+    vNumeroTotalDeAcertosSena INTEGER:=0;
+    
+    vValor NUMBER(12,2);
+
    begin
-      null; --> codar aqui
+    SELECT C.PRIMEIRA_DEZENA
+        INTO vPrimeiraDezena
+        FROM CONCURSO 
+        C WHERE C.IDCONCURSO = 
+        (SELECT MAX(IDCONCURSO) FROM CONCURSO);
+        
+    SELECT C.SEGUNDA_DEZENA
+        INTO vSegundaDezena
+        FROM CONCURSO 
+        C WHERE C.IDCONCURSO = 
+        (SELECT MAX(IDCONCURSO) FROM CONCURSO);
+        
+    SELECT C.TERCEIRA_DEZENA
+        INTO vTerceiraDezena
+        FROM CONCURSO 
+        C WHERE C.IDCONCURSO = pConcurso;
+    
+    SELECT C.QUARTA_DEZENA
+        INTO vQuartaDezena
+        FROM CONCURSO 
+        C WHERE C.IDCONCURSO = pConcurso;
+        
+    SELECT C.QUINTA_DEZENA
+        INTO vQuintaDezena
+        FROM CONCURSO 
+        C WHERE C.IDCONCURSO = pConcurso;
+    
+    SELECT C.SEXTA_DEZENA
+        INTO vSextaDezena
+        FROM CONCURSO 
+        C WHERE C.IDCONCURSO = pConcurso;
+    
+    -- CRIA GANHADORES COM VALOR DO PREMIO ERRADO E ATUALIZA O ACUMULOU PARA 0 SE ALGUEM GANHOU
+    FOR aposta IN C_ListaDeApostas LOOP
+        vIdAposta:=aposta.IDAPOSTA;
+        FOR numero IN C_NumerosAposta LOOP
+            IF vPrimeiraDezena = numero.NUMERO OR vSegundaDezena = numero.NUMERO OR vTerceiraDezena = numero.NUMERO OR vQuartaDezena = numero.NUMERO OR vQuintaDezena = numero.NUMERO OR vSextaDezena = numero.NUMERO THEN
+                vNumeroDeAcertosPorAposta := vNumeroDeAcertosPorAposta + 1;
+            END IF;
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('numero de acertos:'||vNumeroDeAcertosPorAposta ||' id:'||aposta.IDAPOSTA);
+        
+        IF vNumeroDeAcertosPorAposta = 4 THEN
+            SELECT C.PREMIO_QUADRA 
+                INTO vValor
+                FROM CONCURSO C 
+                WHERE C.IDCONCURSO=pConcurso;
+            vNumeroTotalDeAcertosQuadra:=vNumeroTotalDeAcertosQuadra+1;
+        END IF;
+        
+        IF vNumeroDeAcertosPorAposta = 5 THEN
+            SELECT C.PREMIO_QUINA 
+                INTO vValor
+                FROM CONCURSO C 
+                WHERE C.IDCONCURSO=pConcurso;
+            vNumeroTotalDeAcertosQuina:=vNumeroTotalDeAcertosQuina+1;
+        END IF;
+        
+        IF vNumeroDeAcertosPorAposta = 6 THEN
+            SELECT C.PREMIO_SENA 
+                INTO vValor
+                FROM CONCURSO C 
+                WHERE C.IDCONCURSO=pConcurso;
+            vNumeroTotalDeAcertosSena:=vNumeroTotalDeAcertosSena+1;
+        END IF;
+        
+        IF vNumeroDeAcertosPorAposta >= 4 THEN
+            INSERT INTO APOSTA_PREMIADA (IDAPOSTA,ACERTOS,VALOR)
+            VALUES(vIdAposta,vNumeroDeAcertosPorAposta,vValor);
+            
+            UPDATE CONCURSO
+            SET ACUMULOU=0
+            WHERE IDCONCURSO = pConcurso;
+        END IF;
+        
+        vNumeroDeAcertosPorAposta :=0;
+    END LOOP;
+    
+    FOR aposta IN C_ApostasDoConcurso LOOP
+            IF aposta.ACERTOS = 4 THEN
+                SELECT C.PREMIO_QUADRA 
+                    INTO vValor
+                    FROM CONCURSO C 
+                    WHERE C.IDCONCURSO=pConcurso;
+                    
+                    UPDATE APOSTA_PREMIADA
+                    SET VALOR = vValor/vNumeroTotalDeAcertosQuadra
+                    WHERE IDAPOSTA_PREMIADA = aposta.IDAPOSTA_PREMIADA;
+            END IF;
+            
+            IF aposta.ACERTOS = 5 THEN
+                SELECT C.PREMIO_QUINA 
+                    INTO vValor
+                    FROM CONCURSO C 
+                    WHERE C.IDCONCURSO=pConcurso;
+                    
+                    UPDATE APOSTA_PREMIADA
+                    SET VALOR = vValor/vNumeroTotalDeAcertosQuina
+                    WHERE IDAPOSTA_PREMIADA = aposta.IDAPOSTA_PREMIADA;
+            END IF;
+            
+            IF aposta.ACERTOS = 6 THEN
+                SELECT C.PREMIO_SENA 
+                    INTO vValor
+                    FROM CONCURSO C 
+                    WHERE C.IDCONCURSO=pConcurso;
+                    
+                    UPDATE APOSTA_PREMIADA
+                    SET VALOR = vValor/vNumeroTotalDeAcertosSena
+                    WHERE IDAPOSTA_PREMIADA = aposta.IDAPOSTA_PREMIADA;
+            END IF;
+        END LOOP;    
+    
    end atualizaAcertadores;
    
 begin
