068b1ddd90d67ae4419162b1416b33e250131c7f
Pedro Bohlmann Cascaes Silva <pedro.silva@cwi.com.br>
2018-06-26 14:04:07 +0000

    feat(LocadoraVHS): adicionando teste unitario com problema

--- ModuloJava/aula04/oldflix/src/test/java/br/com/cwi/crescer/oldflix/service/cliente/BuscarClientePorIdServiceTest.java
+++ ModuloJava/aula04/oldflix/src/test/java/br/com/cwi/crescer/oldflix/service/cliente/BuscarClientePorIdServiceTest.java
@@ -2,21 +2,15 @@ package br.com.cwi.crescer.oldflix.service.cliente;
 
 import br.com.cwi.crescer.oldflix.dominio.Cliente;
 import br.com.cwi.crescer.oldflix.dominio.repository.IClienteRepository;
-import br.com.cwi.crescer.oldflix.exception.ClienteNaoCadastrado;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
-
-import static org.junit.Assert.*;
 
 //@RunWith(SpringRunner.class) sobe o spring mas para o mockito é outro caso
 @RunWith(MockitoJUnitRunner.class) // roda test com mockito show
@@ -47,7 +41,5 @@ public class BuscarClientePorIdServiceTest {
         Assert.assertNotNull(clienteAchado);
 
         Assert.assertEquals(id,clienteAchado.getId());
-
-
     }
 }
